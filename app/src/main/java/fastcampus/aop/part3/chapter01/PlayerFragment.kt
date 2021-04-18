d86e642286aa998ba8745ae7387d768ea1e01878
kyeongwan <kang@kyeongwan.kr>
2021-04-18 21:12:01 +0900

    clip01,02 MotionLayout 이용하여 화면 전환 UI 구성하기

---
+++ app/src/main/java/fastcampus/aop/part3/chapter01/PlayerFragment.kt
@@ -0,0 +1,52 @@
+package fastcampus.aop.part3.chapter01
+
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.fragment.app.Fragment
+import fastcampus.aop.part3.chapter01.databinding.FragmentPlayerBinding
+import kotlin.math.abs
+
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+
+
+    private var binding : FragmentPlayerBinding? = null
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
+        binding = fragmentPlayerBinding
+
+
+        fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object: MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                binding?.let {
+                    (activity as MainActivity).also { mainActivity ->
+                        mainActivity.findViewById<MotionLayout>(R.id.mainMotionLayout).progress = abs(progress)
+                    }
+                }
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+
+
+        })
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+}
