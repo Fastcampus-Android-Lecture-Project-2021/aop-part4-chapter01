2d5017d446cf85bccf7b749a2fd98461e3af4e77
kyeongwan <kang@kyeongwan.kr>
2021-04-18 22:47:38 +0900

    clip03,04 영상 목록 기본 구조 만들기

---
+++ app/src/main/java/fastcampus/aop/part3/chapter01/dto/VideoDto.kt
@@ -0,0 +1,7 @@
+package fastcampus.aop.part3.chapter01.dto
+
+import fastcampus.aop.part3.chapter01.model.VideoModel
+
+data class VideoDto(
+    val videos: List<VideoModel>
+)
---
+++ app/src/main/java/fastcampus/aop/part3/chapter01/service/VideoService.kt
@@ -0,0 +1,11 @@
+package fastcampus.aop.part3.chapter01.service
+
+import fastcampus.aop.part3.chapter01.dto.VideoDto
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface VideoService {
+
+    @GET("/v3/cbb293eb-e8b2-4079-ba82-472d1c0419d1")
+    fun listVideos(): Call<VideoDto>
+}
